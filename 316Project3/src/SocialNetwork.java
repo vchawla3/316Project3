babb302260b010616332d89237469a7b6c6dbe73
vchawla3 <vchawla3@ncsu.edu>
2015-12-03 22:22:10 -0500

    started socialNetwork

--- 316Project3/src/SocialNetwork.java
+++ 316Project3/src/SocialNetwork.java
@@ -1,11 +1,38 @@
 import java.util.HashMap;
+import java.util.Scanner;
+import java.io.*;
 
 public class SocialNetwork {
 	
-	LinkedList<Person> allPeople = new LinkedList<Person>();
-	HashMap<String, Person> thegraph = new HashMap<String,Person>();
+	//LinkedList<Person> allPeople = new LinkedList<Person>();
+	public static HashMap<String, Person> total = new HashMap<String,Person>();
+	public static int peopleCount = 0;
 	
 	public static void main(String args[]){
+		try {
+			File input = new File(args[0]);
+			Scanner scan = new Scanner(input);
+			
+			createGraph(scan);
+			parseCommands();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not Found!");
+			e.printStackTrace();
+		}
 		
+		
+	}
+	
+	public static void parseCommands(){
+		
+	}
+	
+	public static void createGraph(Scanner scan){
+		while(scan.hasNext()){
+			String name = scan.nextLine();
+			Person p = new Person(name);
+			total.put(name, p);
+			peopleCount++;
+		}
 	}
 }
